14ccb41cfbc4defbd8c68c8534460aee4d32005e
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-03-25 13:07:40 +0530

    updated sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,35 @@ import { MarketStories } from "@/components/MarketStories"
 import { useGeneratorStore } from "@/lib/utils/sideBarState";
 import { useState } from "react";
 
+const navItems = ["Discussion Fourm", "Market"]
+
 export default function Home() {
 
   const { setIsOpen, isOpen } = useGeneratorStore()
-  const [isActive, setIsActive] = useState<"discussion" | "market">("discussion")
+  const [isActive, setIsActive] = useState<"Discussion Fourm" | "Market">("Discussion Fourm")
 
   const handelSideBar = () => {
     setIsOpen(!isOpen)
   }
 
   const handelNav = (id: string) => {
-    setIsActive(id as "discussion" | "market")
+    setIsActive(id as "Discussion Fourm" | "Market")
   }
 
   return (
     <main className="flex min-h-screen overflow-hidden bg-white flex-1 relative">
-      <button onClick={handelSideBar} className="w-fit h-24 bg-[#1E375F] absolute z-10 top-2/4 px-1">▶</button>
+      <button onClick={handelSideBar} className="w-fit h-24 bg-[#1E375F] fixed z-10 top-96 px-1">▶</button>
 
       <div className="lg:hidden w-screen flex flex-col items-center justify-center">
         <div className="w-full flex justify-around bg-blue-900">
-          <button id="discussion" onClick={(e) => handelNav(e.currentTarget.id)} className={`w-6/12 p-3 ${isActive === "discussion" ? "border-b bg-blue-800" : ""}`}>Discussion Forum</button>
-          <button id="market" onClick={(e) => handelNav(e.currentTarget.id)} className={`w-6/12 h-full px-4${isActive === "market" ? "border-b bg-blue-800" : ""}`}>Market Stories</button>
+
+          { navItems.map((item) => (
+            <button id={item} onClick={(e) => handelNav(e.currentTarget.id)} className={`w-6/12 p-3 ${isActive === item ? "border-b-4 bg-blue-800 border-red-600" : ""}`}>{item}</button>
+          ))}
         </div>
 
         <div className="flex flex-col justify-center">
-          {isActive === "discussion" ? <Discussions /> : <MarketStories />}
+          {isActive === "Discussion Fourm" ? <Discussions /> : <MarketStories />}
         </div>
       </div>
 
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,6 +3,7 @@
 'use client'
 
 import { useGeneratorStore } from '@/lib/utils/sideBarState'
+import { relative } from 'path'
 import React from 'react'
 
 const navItems: {
@@ -11,11 +12,11 @@ const navItems: {
 }[] = [
         {
             title: "Discussion Fourm",
-            icon:  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)"  width="30px" height="30px" ><path d="M14.45 19L12 22.5L9.55 19H3C2.44772 19 2 18.5523 2 18V4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V18C22 18.5523 21.5523 19 21 19H14.45Z"></path></svg>
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)" width="30px" height="30px" ><path d="M14.45 19L12 22.5L9.55 19H3C2.44772 19 2 18.5523 2 18V4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V18C22 18.5523 21.5523 19 21 19H14.45Z"></path></svg>
         },
         {
             title: "Market Stories",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  width="30px" height="30px"><path d="M12.0049 22.0027C6.48204 22.0027 2.00488 17.5256 2.00488 12.0027C2.00488 6.4799 6.48204 2.00275 12.0049 2.00275C17.5277 2.00275 22.0049 6.4799 22.0049 12.0027C22.0049 17.5256 17.5277 22.0027 12.0049 22.0027ZM8.50488 14.0027V16.0027H11.0049V18.0027H13.0049V16.0027H14.0049C15.3856 16.0027 16.5049 14.8835 16.5049 13.5027C16.5049 12.122 15.3856 11.0027 14.0049 11.0027H10.0049C9.72874 11.0027 9.50488 10.7789 9.50488 10.5027C9.50488 10.2266 9.72874 10.0027 10.0049 10.0027H15.5049V8.00275H13.0049V6.00275H11.0049V8.00275H10.0049C8.62417 8.00275 7.50488 9.12203 7.50488 10.5027C7.50488 11.8835 8.62417 13.0027 10.0049 13.0027H14.0049C14.281 13.0027 14.5049 13.2266 14.5049 13.5027C14.5049 13.7789 14.281 14.0027 14.0049 14.0027H8.50488Z"></path></svg>
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="30px" height="30px"><path d="M12.0049 22.0027C6.48204 22.0027 2.00488 17.5256 2.00488 12.0027C2.00488 6.4799 6.48204 2.00275 12.0049 2.00275C17.5277 2.00275 22.0049 6.4799 22.0049 12.0027C22.0049 17.5256 17.5277 22.0027 12.0049 22.0027ZM8.50488 14.0027V16.0027H11.0049V18.0027H13.0049V16.0027H14.0049C15.3856 16.0027 16.5049 14.8835 16.5049 13.5027C16.5049 12.122 15.3856 11.0027 14.0049 11.0027H10.0049C9.72874 11.0027 9.50488 10.7789 9.50488 10.5027C9.50488 10.2266 9.72874 10.0027 10.0049 10.0027H15.5049V8.00275H13.0049V6.00275H11.0049V8.00275H10.0049C8.62417 8.00275 7.50488 9.12203 7.50488 10.5027C7.50488 11.8835 8.62417 13.0027 10.0049 13.0027H14.0049C14.281 13.0027 14.5049 13.2266 14.5049 13.5027C14.5049 13.7789 14.281 14.0027 14.0049 14.0027H8.50488Z"></path></svg>
         },
     ]
 
@@ -23,10 +24,10 @@ export const SideBar = () => {
 
     const { isOpen } = useGeneratorStore()
 
-    if (isOpen) {
-        return (
-            <div className={"bg-[#1E375F] min-w-60 lg:w-80"}>
+    return (
+        <div className={`bg-[#1E375F] min-h-screen min-w-60 lg:w-80 ${isOpen ? "fixed overscroll-none" : "relative"}`}>
 
+            <div className='sticky top-0'>
                 <section className='p-5 border-b-2 border-[#364F72] flex items-center justify-between'>
                     <div className='flex items-center gap-3'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)" width="30px" height="30px" >
@@ -51,8 +52,8 @@ export const SideBar = () => {
                     ))
                     }
                 </ul>
-
             </div>
-        )
-    }
+
+        </div>
+    )
 }
